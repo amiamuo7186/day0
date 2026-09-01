de600290603b6a4df765eaf0c6aa31eb0dff2c60
amiamuo7186 <boushmaoum@gmail.com>
2026-09-01 13:54:53 +0100

    les conditions

---
+++ DAY0/condition.js
@@ -0,0 +1,37 @@
+// condition 
+let age =15
+if (age >=18){
+    console.log("majeur")
+} 
+else{
+    console.log( "mineur")
+}
+// condition avec else if 
+if (age >= 25){
+    console.log( "jeune" )
+}
+else if (age>= 15){
+    console.log( "adolescent" )
+}
+else {
+    console.log ("enfant")
+}
+// switch cases 
+day= 5
+switch(day){
+    case 1: console.log ("Lundi")
+     break;
+    case 2: console .log ("Mardi")
+     break;
+    case 3: console.log ("Mercredi")
+     break;
+    case 4: console.log ("Jeudi")
+     break;
+    case 5: console.log ("Vendredi" )
+     break;
+    case 6: console .log ("Samedi")
+     break;
+    case 7 :console .log ("Dimanche" )
+     break;
+    default :console.log ("veuiller enterer une autre valeur"  )
+}
